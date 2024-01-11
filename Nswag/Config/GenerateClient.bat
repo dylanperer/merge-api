61e3c0c87727101855db2439b9ed4375e89a371c
Dylan <dylan.perer29@protonmail.com>
2024-01-11 21:13:41 +1300

    .

--- Nswag/Config/GenerateClient.bat
+++
@@ -1,1 +0,0 @@
-openapi-generator-cli generate -i spec.json -g dart -o ../src/generated_dart_client
---
+++ Nswag/generate-client.bat
@@ -0,0 +1,1 @@
+openapi-generator-cli generate -i ./Config/spec.json -g dart -o ./Src/dart_client
